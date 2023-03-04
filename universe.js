91d0b10b6c7e9cf00029b551f1351342109e3cb6
unknown <apurbahasanj@gmail.com>
2023-03-04 19:25:08 +0600

    card styles and Every problem has been solved

--- universe.js
+++ universe.js
@@ -33,23 +33,22 @@ const displayAi = (data, sortBy = '') => {
   // get all particular data
   data.forEach((ai) => {
     const { published_in, image, name, features, id } = ai;
+    console.log(ai)
     const aiDiv = document.createElement("div");
-    aiDiv.classList.add("col");
+    aiDiv.classList.add("col","card-group");
 
     aiDiv.innerHTML = `
             <div class="card p-3 shadow">
                 <img src="${image}" class="card-img-top mb-3 rounded-3 img-fluid" alt="..." style="height: 300px; width: 437px;">
                 <div class="card-body">
-                    <div>
+                    <div class="h-50">
                         <h5 class=" my-2 fs-3 fw-semibold">Features</h5>
-                        <ol class="text-secondary ps-4">
-                            <li>${features[0]}</li>
-                            <li>${features[1]}</li>
-                            <li>${features[2]}</li>
-                        </ol>
+                        ${features?.length > 0 ?
+                          `<ol class="text-secondary ps-4">${features.map((item) => `<li>${item ?? 'No Data Found'}</li>`).join('')}</ol>` :
+                          'No Data Found'}    
                     </div>
                     <hr>
-                    <div class="d-flex justify-content-between align-items-center">
+                    <div class="d-flex card-footer justify-content-between align-items-center">
                         <div class="">
                             <h5 class=" my-2 fs-3 fw-semibold">${name}</h5>
                             <p id="published-date" class="m-0 text-secondary">
@@ -90,6 +89,7 @@ const loadAiDetails = id => {
   .then(data => showAiDetails(data.data))
 }
 
+// Get ai details btn
 const showAiDetails = aiDetails =>{
   const modalAiDiv = document.getElementById('modal-ai-details');
   
@@ -98,6 +98,7 @@ const showAiDetails = aiDetails =>{
   } = aiDetails;
 
 
+  // ai accuracy function
   const aiAccuracy = () => {
     if (aiDetails.accuracy?.score > 0) {
       return `${aiDetails.accuracy.score * 100}% Accuracy`;
@@ -107,7 +108,7 @@ const showAiDetails = aiDetails =>{
   }
     
   
-  
+  // Ai details modal
   modalAiDiv.innerHTML = `
   <div class="modal-header">
                 <button type="button" class="btn-close bg-danger text-white" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -118,19 +119,23 @@ const showAiDetails = aiDetails =>{
                   <h2 class="card-title fw-semibold mb-4">${description}</h2>
                   <!-- Payment Price -->
                   <div class="d-flex justify-content-around align-items-center mb-4">
-                    <div class=" d-flex fs-5 p-2 fw-semibold justify-content-center align-items-center modal-price-box rounded-4 bg-white text-success">${pricing? pricing[0].price : 'free of cost'}<br>${pricing? pricing[0].plan : 'Basic'}</div>
-
-                    <div class=" d-flex fs-5 p-2 fw-semibold justify-content-center align-items-center modal-price-box rounded-4 bg-white text-warning">${pricing? pricing[1].price : 'free of cost'}<br>${pricing? pricing[1].plan : 'Basic'}</div>
-                    <div class=" d-flex fs-5 p-2 fw-semibold justify-content-center align-items-center modal-price-box rounded-4 bg-white text-danger-emphasis">${pricing? pricing[2].price : 'free of cost'}<br>${pricing? pricing[2].plan : 'Basic'}</div>
+                  <div class="d-flex fs-5 p-2 fw-semibold justify-content-center align-items-center modal-price-box rounded-4 bg-white text-success">
+  ${pricing && pricing[0].price === null && pricing[0].price === undefined && pricing[0].price === 0 ? pricing[0].price : 'Free of Cost/'}
+  <br>
+  ${pricing && pricing[0].plan ? pricing[0].plan : 'Basic'}
+  </div>
+                    <div class=" d-flex fs-5 p-2 fw-semibold justify-content-center align-items-center modal-price-box rounded-4 bg-white text-warning">${pricing? pricing[1].price : 'free of cost/'} <br>${pricing? pricing[1].plan : 'Pro'}</div>
+                    <div class=" d-flex fs-5 p-2 fw-semibold justify-content-center align-items-center modal-price-box rounded-4 bg-white text-danger-emphasis">${pricing? pricing[2].price : 'free of cost/'} <br>${pricing? pricing[2].plan : 'Enterprise'}</div>
                   </div>
                   <div class="row">
                     <div class="col">
                       <h2 class="fw-semibold fs-2 mb-3">Features</h2>
                       <ul>
-                        <li>${features[1].feature_name}</li>
-                        <li>${features[2].feature_name}</li>
-                        <li>${features[1].feature_name}</li>
+                        <li>${features[1]?.feature_name ?? 'No Data Found'}</li>
+                        <li>${features[2]?.feature_name ?? 'No Data Found'}</li>
+                        <li>${features[3]?.feature_name ?? 'No Data Found'}</li>
                       </ul>
+                      
                     </div>
                     <div class="col">
                       <h2 class="fw-semibold fs-2 mb-3">Integrations</h2>
